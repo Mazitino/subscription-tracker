e42efba9addc9efbe5c68959cbec37eb111903a1
Mazitino <aydar.mazitov@gmail.com>
2025-05-15 03:39:15 +0500

    feat(auth): enhance sign-in functionality with error handling and response structure

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,7 @@ import { signUp, signIn, signOut  } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
+// Path: /api/v1/auth/ (POST)
 authRouter.post('/sign-up', signUp);
 authRouter.post('/sign-in', signIn);
 authRouter.post('/sign-out', signOut);
