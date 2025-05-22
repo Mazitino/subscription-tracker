120fe817bb18589b033176bf644da45f7f9037bd
Mazitino <aydar.mazitov@gmail.com>
2025-05-23 03:34:40 +0500

    feat(users): implement user retrieval endpoints with authorization

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express"; 
-
+import { getUsers, getUser } from "../controllers/user.controller.js";
+import authorize from "../middleware/auth.middleware.js";
 const userRouter = Router();  
 
 
 // GET /users -> Get all users
-userRouter.get('/', (req, res) => res.send({ title: 'GET all users' }));
+userRouter.get('/', getUsers);
 
 // GET /users/:id -> Get user by id
-userRouter.get('/:id', (req, res) => res.send({ title: 'GET user details' }));
+userRouter.get('/:id', authorize, getUser);
 
 // POST /users -> Create new user
 userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
